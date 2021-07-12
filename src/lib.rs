7e4cd71bea4b8ec66777bf95abf576a04d7943aa
Klim Tsoutsman <klimusha@gmail.com>
2021-07-12 15:37:29 +1000

    v0.1.1 release

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,22 @@
 //! # use kaitai::include_kaitai;
 //! include_kaitai!("filename");
 //! ```
+use yaml_rust::{YamlLoader, YamlEmitter};
+use std::fs;
 
 #[proc_macro]
 pub fn include_kaitai(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    item
+    let ast = syn::parse_macro_input!(item as syn::Expr);
+
+    let filename = match ast {
+     syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Str(ref s) , ..}) => s.value(),
+        _ => panic!("invalid input"),
+    };
+    eprintln!("{:#?}", std::env::current_dir().unwrap());
+
+    let file_contents = fs::read_to_string(filename).expect("error reading file: ");
+    let structure = &YamlLoader::load_from_str(&file_contents).expect("error parsing file: ")[0];
+
+    eprintln!("{:#?}", structure);
+    todo!()
 }
---
+++ tests/basic.rs
@@ -0,0 +1,5 @@
+use kaitai::include_kaitai;
+
+fn main() {
+    include_kaitai!("tests/test.ksy");
+}
