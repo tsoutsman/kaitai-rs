56116fdba92a1b5ba429476106fb7418e30e945a
Klim Tsoutsman <klimusha@gmail.com>
2021-07-12 16:17:18 +1000

    Update github workflows

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,35 @@
 //! This crate is still very much a work in progress; it does not work.
 //!
 //! A macro for compiling Kaitai Struct into Rust.
+//!
 //! # Syntax
-//! ```
+//! ```dont_run
 //! # use kaitai::include_kaitai;
 //! include_kaitai!("filename");
 //! ```
-use yaml_rust::{YamlLoader, YamlEmitter};
-use std::fs;
+//! # Semantics
+//! The file is located relative to the project's root directory.
 
 #[proc_macro]
 pub fn include_kaitai(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let cloned = item.clone();
     let ast = syn::parse_macro_input!(item as syn::Expr);
 
     let filename = match ast {
-     syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Str(ref s) , ..}) => s.value(),
+        syn::Expr::Lit(syn::ExprLit {
+            lit: syn::Lit::Str(ref s),
+            ..
+        }) => s.value(),
         _ => panic!("invalid input"),
     };
-    eprintln!("{:#?}", std::env::current_dir().unwrap());
 
-    let file_contents = fs::read_to_string(filename).expect("error reading file: ");
-    let structure = &YamlLoader::load_from_str(&file_contents).expect("error parsing file: ")[0];
+    let file_contents = std::fs::read_to_string(filename).expect("error reading file: ");
+    let structure =
+        &yaml_rust::YamlLoader::load_from_str(&file_contents).expect("error parsing file: ")[0];
 
-    eprintln!("{:#?}", structure);
-    todo!()
+    cloned
+}
+
+#[allow(dead_code)]
+struct S { temp: u8,
 }
